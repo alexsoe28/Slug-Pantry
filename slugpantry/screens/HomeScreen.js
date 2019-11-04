2ee1817c1abd138f86796d377c1b9b261674b143
asoe <asoe@ucsc.edu>
2019-11-04 03:13:51 -0800

    Can now delete items from the flatlist

--- slugpantry/screens/HomeScreen.js
+++ slugpantry/screens/HomeScreen.js
@@ -14,7 +14,8 @@ import {
 } from 'react-native';
 
 import { MonoText } from '../components/StyledText';
-import Swipeout from 'react-native-swipeout';
+import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
+
 export default function HomeScreen() {
 
   const [enterIngredient, setEnterIngredient] = useState('');
@@ -30,7 +31,11 @@ export default function HomeScreen() {
       { id: Math.random().toString(), value: enterIngredient }
     ]);
   };
-
+  deleteItemByID = ingredientId =>{
+    setIngredientList(currentIngredients =>{
+      return currentIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+    })
+  }
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
@@ -39,21 +44,27 @@ export default function HomeScreen() {
           style={styles.input}
           onChangeText={IngredientInputHandler}
           value={enterIngredient}
-          />
+        />
         <Button 
           title="+" 
           onPress={addIngredientHandler}
-          />
-        </View>
-        <FlatList 
-          keyExtractor={(item, index) => item.id}
-          data={ingredientList}
-          renderItem={itemData => (
-            <View style={styles.listItem}>
-              <Text>{itemData.item.value}</Text>
-            </View>
-          )}
         />
+      </View>
+          <FlatList 
+            keyExtractor={(item, index) => item.id}
+            data={ingredientList}            
+            renderItem={itemData => (
+              <View style={styles.listItem}>
+                <Text>{itemData.item.value}</Text>  
+                <View style={styles.buttonContainer}>
+                  <Button title="Remove"
+                  color = 'black'
+                  onPress={deleteItemByID.bind(this, itemData.item.id)}
+                  />
+                </View>
+              </View>
+            )}
+          />       
     </View>
   );
 }
@@ -199,10 +210,17 @@ const styles = StyleSheet.create({
     color: '#2e78b7',
   },
   listItem: {
-    padding: 10,
+    padding: 5,
     margin: 3,
-    backgroundColor: 'yellow',
+    backgroundColor: 'lemonchiffon',
     borderColor: 'black',
     borderWidth:1
+  },
+  buttonContainer: {
+    width: 80,
+    height: 40,
+    flexDirection: 'row',
+    alignItems:'flex-end',
+    backgroundColor: 'lightcoral'
   }
 });
