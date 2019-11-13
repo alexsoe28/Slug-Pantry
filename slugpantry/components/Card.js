90dc1d0adcb56c0dee3abdc92d09c9d4ab22126f
William Kudsk <wkudsk@gmail.com>
2019-11-12 20:06:30 -0800

    I have the card set up for one example api call for just chicken. It displays an image, states the name and pressing on the name sends you to the page for the recipe

--- slugpantry/components/Card.js
+++ slugpantry/components/Card.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {StyleSheet, View, Text, Image} from 'react-native';
+import {StyleSheet, View, Text, Image, Linking} from 'react-native';
 
-const Card = props => {
+const Card = props  => {
+    
+    let url = 'http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html';
+    
+    const linkHandler = () => {
+        Linking.openURL(url).catch((err) => console.error('An error occurred', err));
+    };
+    
     return(
         <View style={styles.card}>
             <Image source = {{uri: 'https://www.edamam.com/web-img/e12/e12b8c5581226d7639168f41d126f2ff.jpg'}}
             style={styles.imageStyle} />
-            <Text>Chicken Paprikash</Text>
+            <Text style={styles.titleStyle} onPress={linkHandler}>Chicken Paprikash</Text>
         </View>
     );
 };
@@ -25,14 +32,18 @@ const styles = StyleSheet.create({
         padding: 20,
         borderRadius: 10,
         maxHeight: '20%',
-        justifyContent: 'space-between'
+        //justifyContent: 'space-between',
+        flexDirection: 'row',
+        margin: 10
     },
     titleStyle:{
-        
+        fontSize: 24,
+        fontFamily: 'sans-serif-medium'
     },
     imageStyle:{
-        width: '40%',
-        height: '100%'
+        width: 100,
+        height: 100,
+        marginRight: 10
     }
 });
 
