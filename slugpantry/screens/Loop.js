8e063d0caa172c763c7c3b1d6c1f44611a166d18
Collyn Noda <cnoduh@gmail.com>
2019-11-16 15:28:57 -0800

    started code for powerset loop

---
+++ slugpantry/screens/Loop.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, ActivityIndicator, FlatList} from 'react-native';
+
+import Card from '../components/Card';
+
+
+const Loop = props => {
+
+
+    const [dataSource, setDataSource] = useState(null);
+    const [isLoading,setIsLoading] = useState(true);
+    var ingredients = ["chicken", "banana", "nutella", "beets", "ground beef"];
+    var ingredientSet = new setDataSource(ingredients);
+    var powerSet = [[]];
+    var recipes = [];
+    var images = [];
+    var links = [];
+
+    function powerize(array){
+        for (var i = 0; i < array.length; i++){
+            for (var j = 0; j < powerSet.length; j++){
+                powerSet.push(powerSet[j].concat("+" + array[i]))
+            }
+        }
+    }
+
+    const dataSourceHandler = source => {
+        setDataSource(source);
+    }
+
+    componentDidMount =  async () => {   
+        return fetch('https://api.edamam.com/search?q=chicken&app_id=$e1013624&app_key=$1053882cde32aadeb1a812eb85e3b572')
+        .then(console.log("found API"))
+        .then ((response) => response.json() )
+        .then( (responseJson) => {
+            dataSourceHandler(responseJson.hits);
+            setIsLoading(false);
+        })
+        .catch((error) => {
+            console.log(error)
+        }) 
+    }
+    
+
+    let content = <ActivityIndicator/>
+    if(isLoading){
+        componentDidMount();
+    }
+    if(!isLoading){
+        powerize(ingredients)
+        content = <FlatList
+            keyExtractor={(item, index) => item.id}
+            data={dataSource}
+            renderItem={itemData => (<View style={styles.item}>
+            <Card title={itemData.item.recipe.label}
+                  url={itemData.item.recipe.url}
+                  source={itemData.item.recipe.image}
+                  key={itemData.item.id}/>
+            </View>)}/>;
+    }
+
+
+    return(
+
+        <View style={styles.screen}>
+            {content}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen:{
+        flex: 1
+    }
+});
+
+export default Loop;
